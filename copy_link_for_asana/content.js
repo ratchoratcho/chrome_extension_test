21c6cd5c728a74d16867eafd2d4f6c76567046b8
杉浦光紀 <sugiura_koki@sugiuramitsunorinoMacBook-puro.local>
2018-04-25 17:26:41 +0900

    this is what i want to do

--- copy_link_for_asana/content.js
+++ copy_link_for_asana/content.js
@@ -1,12 +1,12 @@
 /* Listen for messages */
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-  if (msg.command && (msg.command == "change_title")) {
+  if (msg.command && (msg.command == "get_task")) {
     /*var src = document.getElementsByTagName("title")[0].innerHTML;
     var dst = msg.title;
     document.getElementsByTagName("title")[0].innerHTML = dst;
     sendResponse("the page title's changed: '" + src + "' -> '" + dst + "'");*/
-    //var task_name = document.getElementsByClassName('simpleTextarea--dynamic')[0].innerHTML;
-    var task_name = document.getElementsByTagName('title')[0].textContent;
+    var task_name = document.getElementsByClassName('simpleTextarea--dynamic')[0].textContent;
+    //var task_name = document.getElementsByTagName('title')[0].textContent;
     sendResponse(task_name);
   }
 });
