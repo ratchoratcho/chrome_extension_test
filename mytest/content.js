f89f40e7ebd16756668d7ce461ffb01e76a0a0d2
ratchoratcho <ratratchiyo9@gmail.com>
2018-04-27 21:32:31 +0900

    get only task name only on asana page

--- mytest/content.js
+++ mytest/content.js
@@ -1,8 +1,10 @@
-//var content_title = document.getElementsByTagName('title')[0].textContent;
-var task_name = document.getElementsByClassName('simpleTextarea--dynamic simpleTextarea autogrowTextarea')[0].textContent;
+var content_title = document.getElementsByTagName('title')[0].textContent;
+//タスクの名前を取得
+
+var content_title_slice1 = content_title.slice(26, -8);
+
 
 chrome.runtime.sendMessage({
 	//text: "text to be inserted to clipboard!!!!!!"
-	//text : content_title
-	text : task_name
+	text : content_title_slice1
 });
