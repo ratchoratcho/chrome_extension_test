b4e344313d698da82a89c320212e374c2e7077f1
ratchoratcho <ratratchiyo9@gmail.com>
2018-04-29 11:25:03 +0900

    mytest2 not success

---
+++ mytest2/background.js
@@ -0,0 +1,26 @@
+function saveToClipboard(str) {
+    // copy 用に textareaを作る
+    var textArea = document.createElement("textarea");
+    textArea.style.cssText = "position:absolute;left:-100%";
+
+    document.body.appendChild(textArea);
+
+    //textArea.value = "[" + str + "]";
+		textArea.value = str;
+    textArea.select();
+    document.execCommand("copy");
+
+    document.body.removeChild(textArea);
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.runtime.sendMessage({
+		text : "active content.js"
+	});
+	chrome.runtime.onMessage.addListener(
+			function(request, sendler, sentResponse) {
+				var answer = "[" + request.text + "]" + "(" + tab.url + ")";
+				saveToClipboard(answer);
+			}
+	);
+});
