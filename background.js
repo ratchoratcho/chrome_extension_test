f93eaae10e6ef0b949c575d9921f6999b9c091e0
Akito Gyoten <akitogyoten@gmail.com>
2018-05-14 18:35:36 +0900

    update: Asanaのリンクを短縮URLに変換するように修正

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
+
 function saveToClipboard(str) {
-    var textArea = document.createElement("textarea");
+    let textArea = document.createElement("textarea");
     document.body.appendChild(textArea);
 	
 	textArea.value = str;
@@ -13,10 +14,25 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 	  chrome.tabs.sendMessage(tab.id, {text : "active content.js"} );
 });
 
-chrome.runtime.onMessage.addListener(
-		function(request, sender, sentResponse) {
-			   var answer = "[" + request.text + "]" + "(" + sender.tab.url + ")";
-			   saveToClipboard(answer);
-			   return true;
-		}
+chrome.runtime.onMessage.addListener(	
+	function(request, sender, sentResponse) {
+
+		let url = "https://is.gd/create.php?format=simple&url=" + sender.tab.url;
+		let xhr = new XMLHttpRequest();
+		xhr.open('GET', url);
+		xhr.onload = () => {
+			let shorten_url = xhr.responseText;
+
+			let answer = "[" + request.text + "]" + "(" + shorten_url + ")";
+			saveToClipboard(answer);
+			return true;
+		};
+		  xhr.onerror = () => {
+			console.log("error!");
+			return false;
+		};
+		xhr.send();
+	}
 )
+
+
