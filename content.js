ee5f52716ab80a3464e722bec51f5799b00d5768
Akito Gyoten <akitogyoten@gmail.com>
2018-05-30 08:02:23 +0900

    fix: asanaのアップデートに対応

--- content.js
+++ content.js
@@ -1,14 +1,16 @@
 chrome.runtime.onMessage.addListener(
 	function(request, sendler, sentResponse) {
+		const task_title_class = "ProjectPageHeader-projectName--colorNone ProjectPageHeader-projectName";
+
 		//タイトルの名前を取得
 		let content_title = document.getElementsByTagName('title')[0].textContent;		
 
 		//ヘッダー名(プロジェクト名など)を取得
-		let header_element = document.getElementsByClassName('PageHeaderStructure-title')[0]
+		let header_element = document.getElementsByClassName(task_title_class)[0]
 		let header_name = null;
 
 		if (header_element) {
-			header_name = document.getElementsByClassName('PageHeaderStructure-title')[0].textContent;
+			header_name = document.getElementsByClassName(task_title_class)[0].title;
 		}
 
 		let task_name = null;
