b27a7c497ff87f9e843733514a978cb5c9d856ab
Akito Gyoten <akitogyoten@gmail.com>
2018-06-19 18:44:46 +0900

    My Taskのリンクをうまくコピーできない問題を解決

--- content.js
+++ content.js
@@ -1,26 +1,20 @@
 chrome.runtime.onMessage.addListener(
 	function(request, sendler, sentResponse) {
-		const task_title_class = "ProjectPageHeader-projectName--colorNone ProjectPageHeader-projectName";
+		const mypage_title_class_name = "TopbarPageHeaderStructure-title";
+		let mypage_title_element = document.getElementsByClassName("TopbarPageHeaderStructure-title")[0].textContent;
 
 		//タイトルの名前を取得
-		let content_title = document.getElementsByTagName('title')[0].textContent;		
-
-		//ヘッダー名(プロジェクト名など)を取得
-		let header_element = document.getElementsByClassName(task_title_class)[0]
-		let header_name = null;
-
-		if (header_element) {
-			header_name = document.getElementsByClassName(task_title_class)[0].title;
-		}
+		let content_title = document.getElementsByTagName('title')[0].textContent;
 
 		let task_name = null;
-		if (header_name) {
-			task_name = content_title.replace('● ', '').replace(header_name, '').replace(' - Asana', '').replace(' - ', '');
+
+		if (mypage_title_element) {
+			task_name = content_title.replace('● ', '').replace(mypage_title_element, '').replace(' - Asana', '').replace(' - ', '');
 		} else {
 			task_name = content_title.replace('● ', '').replace("Search", '').replace(' - Asana', '').replace(' - ', '');
 		}
-		
+
 		chrome.runtime.sendMessage({ text : task_name });
 		return true;
-}
+	}
 );
